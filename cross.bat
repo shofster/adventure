aefa056a84c1fdf4da4341d042097ee0b30b145c
shofster <shofster@gmail.com>
2023-05-21 11:33:02 -0600

    fyne 2.3.4

--- cross.bat
+++ cross.bat
@@ -1,7 +1,9 @@
 @rem  --pull
-fyne-cross windows -arch=amd64  -icon=building.png
-fyne-cross linux -arch=amd64 -icon=building.png
-fyne-cross linux -arch=arm64 -icon=building.png
-fyne-cross linux -arch=arm -icon=building.png
+fyne-cross windows -arch=amd64  -icon=building.png -app-id=com.scsi.adventure
+fyne-cross linux -arch=amd64 -icon=building.png -app-id=com.scsi.adventure
+fyne-cross linux -arch=arm64 -icon=building.png -app-id=com.scsi.adventure
+fyne-cross linux -arch=arm -icon=building.png -app-id=com.scsi.adventure
 
 
+fyne build
+strip adventure.exe
