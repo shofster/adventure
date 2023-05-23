ddb3ebfb6285041cb6cdfad373d0b405a9b4f9fa
shofster <shofster@gmail.com>
2023-05-23 12:46:02 -0600

    use fyne package command

--- cross.bat
+++ cross.bat
@@ -4,6 +4,4 @@ fyne-cross linux -arch=amd64 -icon=building.png -app-id=com.scsi.adventure
 fyne-cross linux -arch=arm64 -icon=building.png -app-id=com.scsi.adventure
 fyne-cross linux -arch=arm -icon=building.png -app-id=com.scsi.adventure
 
-
-fyne build
-strip adventure.exe
+fyne package --release --icon=building.png --id=com.scsi.adventure
